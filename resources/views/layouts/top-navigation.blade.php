05e9156a2b3613ed5ea7bd387d8718d05218e4c2
C.L.P.T <chunglyphutoan@gmail.com>
2020-02-28 23:11:52 +0700

    Fix some bug

--- resources/views/layouts/top-navigation.blade.php
+++ resources/views/layouts/top-navigation.blade.php
@@ -88,7 +88,7 @@
         <div class="flex items-center px-5 sm:px-6">
           <div class="flex-shrink-0">
             <img class="h-10 w-10 rounded-full"
-                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                 src="https://www.gravatar.com/avatar/{{ md5(Auth::user()->email) }}?&s=32"
                  alt=""/>
           </div>
           <div class="ml-3">
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('threads.index'));
 });
 
 Auth::routes();
